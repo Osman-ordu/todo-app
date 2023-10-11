5ff6f9620fb95712b93d296c961382941b1ad3f2
Osman.ordu <orduosmann@gmail.com>
2023-10-11 22:16:01 +0300

    Added submit handler for 'todo add' feature

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo'
 
 export default function App() {
-  
+
   const [todos,setTodos] = useState([
     {text: 'buy coffee', key:'1'},
     {text: 'create an app', key:'2'},
@@ -17,11 +18,20 @@ export default function App() {
     });
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header/>
       <View style={styles.content}>
-      {/* to form */}
+      <AddTodo submitHandler = {submitHandler}/>
       <View style={styles.list}></View>
       <FlatList
       data={todos}
